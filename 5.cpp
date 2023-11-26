6a2120e0fd07e59ada2e35a9c651719c38ba6f2d
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-26 20:57:05 +0530

    Search in LinkedList

---
+++ 5.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node*next;
+
+    Node(int data){
+        this->data=data;
+        next=nullptr;
+    }
+
+};
+
+Node* convertArraytoLL(vector<int>arr){
+
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+
+    for (int i = 1; i < arr.size(); ++i) {
+        Node* temp = new Node(arr[i]);
+        current->next = temp;
+        current = current->next;
+       // current = temp;
+    }
+     return head;
+}
+
+int SearchinLL(Node*head,int val){
+    Node *temp= head;
+    while(temp){
+      if(temp->data==val) return 1;
+      temp =temp->next;
+    }
+    return 0;
+}
+
+int main(){
+    vector<int>arr={2,5,8,7};
+    Node* head = convertArraytoLL(arr);
+    cout<<SearchinLL(head,2);
+}
