287aace11f1b6a3d4e301ac4d175f987a248485e
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-30 23:09:53 +0530

    Convert Array to LinkedList

--- 02.cpp
+++ 02.cpp
@@ -13,19 +13,21 @@ class Node{
     
 };
 
-Node* convertArraytoLL(vector<int>arr){
+Node *convertArraytoLL(vector<int> arr){
 
-    Node* head = new Node(arr[0]);
-    Node* current = head;
+    if (arr.empty()) return NULL;
+    
+    Node *head = new Node(arr[0]);
+    Node *current = head;
 
-    for (int i = 1; i < arr.size(); ++i) {
-        Node* temp = new Node(arr[i]);
+    for (int i = 1; i < arr.size(); ++i){
+        Node *temp = new Node(arr[i]);
         current->next = temp;
         current = current->next;
-       // current = temp;
+        // current = temp;
     }
-     return head;
-}
+    return head;
+} 
 
 int main(){
     vector<int>arr={2,5,8,7};
