06e1d228a430d1be6585d1c4370e79d2d34d22d7
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-12-01 22:00:58 +0530

    Convert Array to DoublyLinkedList

--- 15.cpp
+++ 15.cpp
@@ -40,6 +40,26 @@ Node* convertArraytoDLL(vector<int> arr) {
    return head;
 }
 
+
+/*  
+ Node* convertArraytoDLL(vector<int> arr) {
+    if (arr.empty()) return nullptr;
+
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+
+    for (int i = 1; i < arr.size(); ++i) {
+        Node* temp = new Node(arr[i],NULL,current);
+        current->next = temp;
+        current = current->next;
+       //current = temp;
+    }
+
+   return head;
+}
+*/
+
+
 int main() {
     vector<int> arr = {2, 5, 8, 7};
     Node* head =convertArraytoDLL(arr);
