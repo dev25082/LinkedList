37d44d013ca7b07b3ac6c3811f2462d29d5b9b62
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-28 12:58:30 +0530

    Check if a LinkedList is Palindrome or Not

--- 32.cpp
+++ 32.cpp
@@ -22,3 +22,41 @@ T.C. = O(2N)
 S.C. = O(N)
 
   
+Approach 2 
+   
+# include <bits/stdc++.h>
+
+Node* reverseLinkedList(Node* head) {
+    if (head == NULL || head->next == NULL)  return head; 
+    Node* newHead = reverseLinkedList(head->next);
+    Node* front = head->next;
+    front->next = head;
+    head->next = NULL;
+    return newHead;
+}
+
+bool isPalindrome(Node* head) {
+    if (head == NULL || head->next == NULL) return true;
+    Node* slow = head;
+    Node* fast = head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;  
+        fast = fast->next->next;  
+    }
+    Node* newHead = reverseLinkedList(slow->next);
+    Node* first = head;  
+    Node* second = newHead; 
+    while (second != NULL) {
+        if (first->data != second->data) {
+            reverseLinkedList(newHead); 
+            return false;
+        }
+        first = first->next;
+        second = second->next;  
+    }
+    reverseLinkedList(newHead);  
+    return true;  
+}
+
+T.C. = O(2N)
+S.C. = O(1)
