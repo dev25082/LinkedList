d1b129145788f4cc28828cf99b1c878311a5166d
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-29 10:50:47 +0530

    Add 1 to a number represented by LinkedList

--- 33.cpp
+++ 33.cpp
@@ -42,3 +42,31 @@ Node *addOne(Node *head)
 
 T.C. = O(3N)
 S.C. = O(1)
+
+
+
+Approach 2: backtracking
+    
+    int addhelper(Node*temp){
+    if(temp==NULL) return 1;
+    int carry=addhelper(temp->next);
+    temp->data+=carry;
+    if(temp->data<10) return 0;
+    temp->data = 0;
+    return 1;
+}
+
+Node *addOne(Node *head)
+{
+    int carry = addhelper(head);
+    if(carry==1){
+        Node *newNode = new Node(1);
+        newNode->next=head;
+        head=newNode;
+    }
+    return head;
+}
+
+
+T.C. = O(N)
+S.C. = O(N)
