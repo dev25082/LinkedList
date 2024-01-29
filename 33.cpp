e5e31b437bf3b159d5fae21187f917471ece0f26
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-29 09:35:05 +0530

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,44 @@
+Approach 1:
+
+
+Node *reverse(Node *head){
+    if(head==NULL || head->next==NULL) return head;
+    Node *temp=head,*prev=NULL;
+    while(temp){
+      Node*front=temp->next;
+      temp->next=prev;
+      prev=temp;
+      temp=front;
+  }
+    return prev;
+}
+
+Node *addOne(Node *head)
+{
+    head=reverse(head);
+    Node *temp=head;
+    int carry=1;
+    while(temp){
+        temp->data = temp->data + carry;
+        if(temp->data<10){
+            carry=0;
+            break;
+        }
+        else{
+            temp->data=0;
+            carry=1;
+        }
+        temp=temp->next;
+    }
+    if(carry){
+        Node *newNode= new Node(1);
+        head=reverse(head);
+        newNode->next=head;
+        return newNode;
+    }
+    head=reverse(head);
+    return head;
+}
+
+T.C. = O(3N)
+S.C. = O(1)
